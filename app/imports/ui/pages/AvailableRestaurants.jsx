dd93a73149d03fbf6e8f20064f11bf874a5ddd4b
rileyts <49923607+rileyts@users.noreply.github.com>
2020-12-15 15:01:36 -1000

    now parse serviceHours manually

--- app/imports/ui/pages/AvailableRestaurants.jsx
+++ app/imports/ui/pages/AvailableRestaurants.jsx
@@ -7,15 +7,27 @@ import { Restaurants } from '../../api/restaurant/Restaurant';
 import Restaurant from '../components/Restaurant';
 
 function isOpen(restaurant, date) {
+  const times = [];
   /** All restaurants are closed on weekends */
   if (date.getDay() === 6 || date.getDay() === 0) {
     return false;
   }
   /** Parse serviceHours string and extract hours and min for opening and closing */
-  const times = restaurant.serviceHours.match(/\d+/g).map(Number);
+  for (let i = 0; i < restaurant.serviceHours.length; i++) {
+    let num;
+    if (restaurant.serviceHours[i] >= '0' && restaurant.serviceHours[i] <= '9') {
+      num = restaurant.serviceHours.charCodeAt(i) - 48;
+      if (restaurant.serviceHours[i + 1] >= '0' && restaurant.serviceHours[i + 1] <= '9') {
+        num = (restaurant.serviceHours.charCodeAt(i) - 48) * 10 + (restaurant.serviceHours.charCodeAt(i + 1) - 48);
+        i++;
+      }
+      times.push(num);
+    }
+  }
+  console.log(times);
   /** Determine if restaurant closed and if its not return that its open */
   /** Restaurant is hour(s) away from being open */
-  if (date.getHours() < times[0]) {
+  if (date.getHours() < times[0] || restaurant.serviceHours === 'Closed') {
     return false;
   }
   /** Restaurant is minute(s) away from being open */
@@ -23,14 +35,12 @@ function isOpen(restaurant, date) {
     return false;
   }
   /** Restaurant closed hour(s) ago */
-  if (date.getHours() > (times[2] + 12) ) {
+  if (date.getHours() > (times[2] + 12)) {
     return false;
   }
   /** Restaurant closed minutes(s) ago */
-  if ((times[2] + 12) === date.getHours() && times[3] > date.getMinutes()) {
-    return false;
-  }
-  return true;
+  return !((times[2] + 12) === date.getHours() && times[3] < date.getMinutes());
+
 }
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
@@ -45,6 +55,7 @@ class AvailableRestaurants extends React.Component {
   renderPage() {
     const d = new Date();
     const available = []; /** if restaurant is both open and not closed */
+    console.log(d.getHours());
     for (let i = 0; i < this.props.restaurants.length; i++) {
       if (isOpen(this.props.restaurants[i], d)) {
         available.push(this.props.restaurants[i]);
